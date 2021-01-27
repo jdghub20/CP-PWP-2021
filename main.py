8eda9259731107fe8729baac4e3935848daf059c
Joe Dias <joedias@hotmail.com>
2021-01-27 03:29:22 +0000

    Learnt what is a dictionary and how to manipulate dictionaries'

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+
 # Lets get max number froma list
 
 #invoices = [6, 10, 20, 47, 69, 11, 50, 230, 20, 24, 39, 29, 45]
@@ -31,15 +32,31 @@
 #   print(movie[1])
 
 # create a function to see a movie specific to a rating
-our_movies = [("Interstellar", 10), ("Ironman", 8.2), ("It Follows", 8.0), ("X-Men", 2)]
+# our_movies = [("Interstellar", 10), ("Ironman", 8.2), ("It Follows", 8.0), ("X-Men", 2)]
 
 
-def rating_filter(movies, max_rating):
-  final_movies = []
+# def rating_filter(movies, max_rating):
+#   final_movies = []
 
-  for movies in movies:
-    if movies[1] >= max_rating:
-      final_movies.append(movies[0])
-  return final_movies    
+#   for movies in movies:
+#     if movies[1] >= max_rating:
+#       final_movies.append(movies[0])
+#   return final_movies    
 
-print(rating_filter(our_movies, 1))
+# print(rating_filter(our_movies, 1))
+
+# learning about Dictionary
+
+# phone_book = {"John": "444-222-4444", "Billy": "444-111-2222"}
+# print(phone_book)
+
+movies = {"Shaw Shank Redemption": 9.7, "The God Father": 8.0}
+
+# Authors blogs stored in a variable
+
+authors = {"Rafeh Qazi": [ "b1", "cleverprogrammer.com/blog/2"], "Naz": [ "b1", "cleverprogrammer.com/blog/3"]}
+
+# to print just b1 value of Naz
+# print(authors["Naz"][0]) -- b1 associated with Key Naz
+# print(authors["Rafeh"][1]) -- "cleverprogrammer.com/blog/2 associated with Key Rafeh
+
