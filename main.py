5e36f642b95e95a5b9f53f29c479f78c3ad00a8f
Joe Dias <joedias@hotmail.com>
2021-01-27 04:00:19 +0000

    'Learnt about how to use the split function to loop over a disctionary object'

--- main.py
+++ main.py
@@ -50,13 +50,46 @@
 # phone_book = {"John": "444-222-4444", "Billy": "444-111-2222"}
 # print(phone_book)
 
-movies = {"Shaw Shank Redemption": 9.7, "The God Father": 8.0}
+# movies = {"Shaw Shank Redemption": 9.7, "The God Father": 8.0}
 
-# Authors blogs stored in a variable
+# # Authors blogs stored in a variable
 
-authors = {"Rafeh Qazi": [ "b1", "cleverprogrammer.com/blog/2"], "Naz": [ "b1", "cleverprogrammer.com/blog/3"]}
+# authors = {"Rafeh Qazi": [ "b1", "cleverprogrammer.com/blog/2"], "Naz": [ "b1", "cleverprogrammer.com/blog/3"]}
 
 # to print just b1 value of Naz
 # print(authors["Naz"][0]) -- b1 associated with Key Naz
 # print(authors["Rafeh"][1]) -- "cleverprogrammer.com/blog/2 associated with Key Rafeh
 
+# Create a Wrod frequency counter
+# In dictionary key are unique
+# Info is not order
+# List is good for maitain order
+
+# final_answer = {"hey": 1, "Hey": 1, "what": 2, "are": 1, "you": 1, "doing": 1, "there": 1}
+
+sentence = "Hey what woop woop wop wop di dop dop there what are you doing"
+
+
+
+# for word in sentence.split():
+#   # if I am finding it for the first time, create a key with that word and set its value to 1
+
+#   # If I have already seen the word... Then increment its' value by 1
+
+#   if word not in final_dictionary:
+#       final_dictionary[word] = 1
+#   else:
+#       final_dictionary[word] = final_dictionary[word] + 1
+
+# print(final_dictionary)        
+
+def word_frequency(sentence):
+  final_dictionary ={}
+  for word in sentence.split():
+    if word not in final_dictionary:
+      final_dictionary[word] = 1
+  else:
+      final_dictionary[word] += 1
+  return final_dictionary 
+
+print(word_frequency(sentence))
